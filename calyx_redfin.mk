acf1022e56db74f804ce28828c5ef235a70c8699
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit 00ac1e0a6a818d7a15caabdd61d8cb7a28eba7f6
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:16:43 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637
        (cherry picked from commit 8ab923bd93da2e3c1eaefb18ae5675b21731e897)
    
    commit 06d9c64a76a20818ce6f0d455f8a46e435b953de
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Mon Jan 25 18:01:02 2021 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: I6c9985670590796ccd2880510a7d5569ca67224e
        (cherry picked from commit 2e6be57f7701cbd775a607b487a814a7d23ebafa)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit 15e6187c21cd95c10d124417334472f2e38ff7db)

---
+++ calyx_redfin.mk
@@ -0,0 +1,22 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Device specific overlay
+DEVICE_PACKAGE_OVERLAYS += device/google/redfin/overlay-calyx
+
+# Inherit device configuration
+$(call inherit-product, device/google/redbull/device-calyx.mk)
+$(call inherit-product, device/google/redfin/aosp_redfin.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_redfin
+PRODUCT_MODEL := Pixel 5
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=redfin \
+    PRIVATE_BUILD_DESC="redfin-user 13 TP1A.220624.014 8819323 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:13/TP1A.220624.014/8819323:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
