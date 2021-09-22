c51c28fb9d0ce8f9afe81dffa070772603567108
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:09:51 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61

--- calyx_redfin.mk
+++ calyx_redfin.mk
@@ -14,6 +14,12 @@ PRODUCT_NAME := calyx_redfin
 PRODUCT_MODEL := Pixel 5
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=redfin \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/redfin
