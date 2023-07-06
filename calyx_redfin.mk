7139308d5e28db7df5b76edb24aed008a3a208b0
Chirayu Desai <chirayudesai1@gmail.com>
2023-07-06 17:29:12 +0530

    Update fingerprint/build description from TQ3A.230705.001
    Change-Id: I907305477eb906305807758df6db2f4a4682da7e

--- calyx_redfin.mk
+++ calyx_redfin.mk
@@ -16,9 +16,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=redfin \
-    PRIVATE_BUILD_DESC="redfin-user 13 TQ3A.230605.011 10161073 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 13 TQ3A.230705.001 10216780 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:13/TQ3A.230605.011/10161073:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:13/TQ3A.230705.001/10216780:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
