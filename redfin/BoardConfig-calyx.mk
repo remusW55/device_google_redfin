ba9f713274ed59308bb142356d26615c4afea42e
RW55 <remusw55@gmail.com>
2024-01-12 19:41:48 +0000

    Temporarily disable the platform security patch date to fix the error: cannot assign to readonly variable: PLATFORM_SECURITY_PATCH

--- redfin/BoardConfig-calyx.mk
+++ redfin/BoardConfig-calyx.mk
@@ -7,7 +7,7 @@
 include device/google/redbull/BoardConfig-calyx.mk
 
 BOOT_SECURITY_PATCH := 2023-11-05
-PLATFORM_SECURITY_PATCH := 2023-11-05
+# PLATFORM_SECURITY_PATCH := 2023-11-05
 VENDOR_SECURITY_PATCH := 2023-11-05
 
 include vendor/google/redfin/BoardConfigVendor.mk
